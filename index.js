f116fd1c2acd856aa5627aca9a67dc596ca1e5ea
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-04-18 12:27:49 +0600

    complete-website-server Assignment-11

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qagzg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
 
@@ -12,6 +14,10 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+client.connect(err => {
+    const customerCollection = client.db(`${process.env.DB_NAME}`).collection("customers");
+        console.log('database connected successfully');
+  });
 
 app.get('/', (req, res) => {
     res.send("hello from db it's working working")
